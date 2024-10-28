2dd7609341873f5570981bf4f558c00b4a3be6f7
DaoOfBullshit <max.bender31@gmail.com>
2024-10-27 21:37:18 -0400

    added auto spin feature

--- main.py
+++ main.py
@@ -46,18 +46,18 @@ def load_balance():
 
 def initial_balance(balance):
     while True:
-        initial_balance = input("Please enter amount of funds: ")
+        initialBalance = input("Please enter amount of funds: ")
         try:
-            initial_balance = float(initial_balance)
-            if initial_balance <= 0:
+            initialBalance = float(initialBalance)
+            if initialBalance <= 0:
                 print("Please enter amount greater than 0... ")
                 continue
-            balance = initial_balance
-            balance = save_balance(balance)
+            balance = initialBalance
+            save_balance(balance)
             break
         except ValueError:
             print("Please enter a number... ")
-    return initial_balance
+    return initialBalance
 
 
 def save_choice(balance):
@@ -75,19 +75,7 @@ def save_choice(balance):
     return balance
 
 
-def main():
-    print("\nWelcome to the Slot Machine!!!")
-    print("Symbols: 🍒 🍓 🍉 🍌 🤑")
-
-    balance = load_balance()
-
-    if balance != 0:
-        balance = save_choice(balance)
-
-    if balance == 0:
-        balance = initial_balance(balance)
-        save_balance(balance)
-
+def manual_spin(balance):
     while balance > 0:
         print(f"\nCurrent Balance: ${balance:,.2f}")
         bet = input("Enter bet amount (or 'q' to quit): ")
@@ -108,8 +96,8 @@ def main():
         if bet > balance:
             print("Insufficient Funds... \n")
             continue
-        elif bet <= 0:
-            print("Please enter amount greater than 0... \n")
+        elif bet < 0.01:
+            print("Minimum Bet Amount is 1 cent... \n")
             continue
 
         balance -= bet
@@ -130,8 +118,125 @@ def main():
         if balance == 0:
             print(f"\nBetter Luck Next Time! \nCurrent Balance: ${balance:,.2f}")
             break
+    return balance
+
+
+def auto_spin(balance):
+    stats = {"Wins": 0,
+             "Losses": 0,
+             "Net Gain": 0,
+             "Symbol Wins": {"🍒": 0, "🍓": 0, "🍉": 0, "🍌": 0, "🤑": 0}}
+
+    startBalance = balance  # setting start balance for later calculation
+
+    # prompt user to input bet of either 10, 100, or 1000
+    while True:
+        try:
+            bet = int(input("Choose bet amount (10, 100, 1000): "))
+            if bet in [10, 100, 1000]:
+                break
+            else:
+                print("Please enter one of the specified values... ")
+        except ValueError:
+            print("Please enter one of the specified values... ")
+
+    # Prompt user for number of rounds
+    while True:
+        roundsChoice = input("Enter a specified number of rounds or type 'max' to spin until balance is empty: ")
+        if roundsChoice.lower() == "max":
+            rounds = float("inf")
+            break
+        else:
+            try:
+                rounds = int(roundsChoice)
+                break
+            except ValueError:
+                print("Please enter a specified number of rounds or type 'max'... ")
+
+    spins = 0  # counter for num of spins
+
+    # Start spinning either for the specified rounds or until balance reaches zero
+    while balance >= bet and spins < rounds:
+        spins += 1
+        balance -= bet
+        row = spin_row()
+        payout = get_payout(row, bet)
+
+        if payout > 0:
+            stats["Wins"] += 1
+            balance += payout
+            symbolWin = row[0]
+            stats["Symbol Wins"][symbolWin] += 1
+        else:
+            stats["Losses"] += 1
+
+    # stats calculations
+    winPct = (stats["Wins"] / spins) * 100 if spins > 0 else 0
+    endBalance = balance
+    stats["Net Gain"] = endBalance - startBalance
+
+    symbolMode = max(stats["Symbol Wins"], key=stats["Symbol Wins"].get)
+    symbolModeCount = stats["Symbol Wins"][symbolMode]
+
+    # create summary txt file
+    with open("Stats Summary.txt", "w", encoding='utf-8') as file:
+        file.write(
+            f"Wins: {stats["Wins"]}"
+            f"\nLosses: {stats["Losses"]}"
+            f"\nWin Percentage: {winPct:.2f}%"
+            f"\n1000x hits: {stats["Symbol Wins"]["🤑"]}"
+            f"\nMost Frequent Symbol: {symbolMode}"
+            f"\n  Number of times hit: {symbolModeCount}"
+            f"\n\nNet Gain/Loss: ${stats["Net Gain"]:,.2f}"
+            f"\nFinal Balance: ${balance:,.2f}"
+        )
 
     save_balance(balance)
+    return balance
+
+
+def choose_mode(balance):
+    while True:
+        mode = input("""
+Select '1' or '2' to choose mode
+    1. Auto Spin (has stats summary)
+    2. Manual Spin
+    Input: """)
+        try:
+            mode = int(mode)
+            if mode == 1:
+                balance = auto_spin(balance)
+                break
+            elif mode == 2:
+                balance = manual_spin(balance)
+                break
+            else:
+                print("Please select either '1' or '2'... ")
+        except ValueError:
+            print("Please select either '1' or '2'... ")
+
+    return balance
+
+
+def main():
+    print("\nWelcome to the Slot Machine!!!")
+    print("Symbols: 🍒 🍓 🍉 🍌 🤑")
+
+    balance = load_balance()
+
+    if balance != 0:
+        balance = save_choice(balance)
+
+    if balance == 0:
+        balance = initial_balance(balance)
+        save_balance(balance)
+
+    print(f"Balance: ${balance:,.2f}")
+
+    balance = choose_mode(balance)
+    print(f"""
+    Thanks for Playing!
+    Remaining Balance: ${balance:,.2f}""")
 
 
 if __name__ == "__main__":
